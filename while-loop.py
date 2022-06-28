91243be4cd161af2376b3da53806a487c2ccfcdc
Hassam-Ahmed! <108329037+Hassam-Ahmed@users.noreply.github.com>
2022-06-28 06:16:12 -0700

    Add files via upload
    While loops in python / simple  program to get  age and name from user using while loop.

---
+++ while-loop.py
@@ -0,0 +1,11 @@
+Age = ""
+name = ""
+while len(Age) == 0:
+   Age = str(input("Enter your age : "))
+
+
+while len(name) == 0:
+   name = str(input("Enter your name : "))
+
+
+print("Hello "+name+" you are "+Age+" years old :)")
